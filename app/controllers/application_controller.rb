47e7c879af7713714a06b4821c6efbb698f775be
jophinjohn <jophinjohn@hotmail.com>
2017-01-31 09:40:26 +0000

    Trial 9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
-         redirect_to root_path
+         redirect_to '/index.html'
   end
 end
 
