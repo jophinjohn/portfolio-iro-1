905ce1e42171a9cea927e05261146b9b569557e4
jophinjohn <jophinjohn@hotmail.com>
2017-01-31 11:30:51 +0000

    Commited

--- _site/app/controllers/application_controller.rb
+++ _site/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
-    redirect_to root_path
+     render 'portfolio_iro/index.html'
   end
 end
 
