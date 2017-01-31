92f9c9362992d06f4d61a2eecaaa018b95109185
jophinjohn <jophinjohn@hotmail.com>
2017-01-31 09:13:58 +0000

    Trial 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
-         redirect_to '/index.html'
+    redirect_to root_path
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+     root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,3 +54,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
